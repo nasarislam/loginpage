a7887eb079f8cdb550bf25a2a496c1e5427636db
Nasar Islam <nasarislamy51l@gmail.com>
2023-08-08 21:55:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import Image from './Untitled design.png'
+// import Logo from './Pink Circle Gradient Photography Logo.jpg'
 import './App.css';
+import {AiOutlineUser} from "react-icons/ai"
+import {LiaLockSolid} from "react-icons/lia"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <img src={Logo} alt="" /> */}
+      <div className="main">
+        <div className="child">
+          <img src={Image} alt="" className='childimg'/>
+        </div>
+        <div className="vl"></div>
+        <div className="child">
+          <div className="login">
+          <span className='title'>Login as a Admin user</span>
+          <form action="#">
+          <div className='inputfield'>
+          <div className='email'>
+          <input type="text" placeholder='nasar@gmail.com'/>
+          <span className='mail'> <AiOutlineUser /> </span>
+          </div>
+          <div className='password'>
+          <input type="password" placeholder='xxxxx'/>
+          <span className='pass'> <LiaLockSolid/></span>
+          </div>
+          </div>
+          <button>LOGIN</button>
+          </form>
+          <span className='foot'>Forget you Password?</span>
+          <span className='foot get'>Get help Signed in.</span>
+          <span className="terms">Terms of use.Privacy policy</span>
+        </div>
+        </div>
+      </div>
     </div>
   );
 }
